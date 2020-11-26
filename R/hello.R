12f4833ab1864c0b12a850b12fe3ac296031f690
vanhungtran <tranhungydhcm@gmail.com>
2020-11-26 20:12:38 +0100

    Vers 0.0.1

--- R/hello.R
+++ R/hello.R
@@ -8,11 +8,9 @@
 #   http://r-pkgs.had.co.nz/
 #
 # Some useful keyboard shortcuts for package authoring:
-#ffffffff
+#
 #   Install Package:           'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-}
+
